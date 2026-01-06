461dfe66278c99f7e5a6613727a7d6e248616c1a
RAMJIT MOURYA <98334037+NUCLIE-X@users.noreply.github.com>
2026-01-06 05:41:43 +0530

    Create products.js

---
+++ src/products.js
@@ -0,0 +1,9 @@
+const products = [
+  { id: 1, name: "Galaxy A14", price: 350, category: "phone" },
+  { id: 2, name: "iPhone SE", price: 450, category: "phone" },
+  { id: 3, name: "Pixel 6a", price: 499, category: "phone" },
+  { id: 4, name: "OnePlus Nord", price: 399, category: "phone" },
+  { id: 5, name: "MacBook Air M1", price: 999, category: "laptop" }
+];
+
+export default products;
